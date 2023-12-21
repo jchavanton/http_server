56def3af945422a4ab5ba95ac49a433fa7515d80
Julien Chavanton <jchavanton@gmail.com>
2023-12-21 14:16:49 -0500

    source

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+// Compile templates on start of the application
+var templates = template.Must(template.ParseFiles("public/upload.html"))
+
+// Display the named template
+func display(w http.ResponseWriter, page string, data interface{}) {
+	templates.ExecuteTemplate(w, page+".html", data)
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	// Maximum upload of 16 MB files
+	r.ParseMultipartForm(16 << 20)
+
+	// Get handler for filename, size and headers
+	file, handler, err := r.FormFile("mediaFileName")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create file
+	dst, err := os.Create("upload/" + handler.Filename)
+	defer dst.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+func checkFileExists(filePath string) bool {
+	_, error := os.Stat(filePath)
+	return !errors.Is(error, os.ErrNotExist)
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	ua := r.Header.Get("User-Agent")
+	m := "upload"
+	fmt.Printf("[%s] %s...\n", ua, m)
+	switch r.Method {
+	case "GET":
+		display(w, "upload", nil)
+	case "POST":
+		uploadFile(w, r)
+	}
+}
+
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	ua := r.Header.Get("User-Agent")
+	m := "download"
+	fn := r.URL.Query().Get("fn")
+	if fn == "" {
+		fmt.Printf("[%s]%s: missing fn param\n", ua, m)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	filePath := "upload/" + fn
+	isFileExist := checkFileExists(filePath)
+	if !isFileExist {
+		fmt.Printf("[%s]%s[%s] file not found\n", ua, m, fn)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	fileBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("[%s]%s[%s] error reading file\n", ua, m, fn)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("[%s]%s[%s]\n", ua, m, fn)
+	w.Header().Set("Content-Disposition", "attachment; filename="+fn)
+	// w.Header().Set("Content-Type", "application/octet-stream")
+	w.WriteHeader(http.StatusOK)
+	w.Write(fileBytes)
+	return
+}
+
+func main() {
+	// Upload route
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/download", downloadHandler)
+
+	//Listen on port 8080
+	fmt.Printf("Listen on port 8080\n")
+	http.ListenAndServe(":8080", nil)
+}
