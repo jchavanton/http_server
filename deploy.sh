5f13fbfa4e0ed8663d4a91a7d1648665282eb8ef
Julien Chavanton <jchavanton@gmail.com>
2024-05-02 16:23:31 -0400

    hosts config

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 INSTALL_PREFIX="/opt/halo"
 
-declare -a media_server_instances=("pbx.mango.band" "pbx.dead-orbit.com")
+# declare -a media_server_instances=("hostname1" "hostname2")
+source hosts.sh
 
 deploy_media_server_config() {
 	ROLE="media_server"
